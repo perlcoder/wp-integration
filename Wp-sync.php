799741af8e8918435c42eee73628d8a675b80da4
Marco Arthur <arthurpbs@gmail.com>
2019-07-28 21:37:13 -0300

    Include Wp-sync main file
    Write required functions to be separated on inc directory under
    functions.php

--- Wp-sync.php
+++ Wp-sync.php
@@ -1,15 +1,26 @@
 <?php
 /**
- * Plugin Name:     Wp Sync
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
- * Text Domain:     Wp-sync
- * Domain Path:     /languages
- * Version:         0.1.0
+ * Plugin Name:       Synchronizer
+ * Plugin URI:        https://yhello.co
+ * Description:       Sync contents from child sites to a central one. Also does few more things.
+ * Version:           1.0
+ * Author:            Yhello
+ * Author URI:        https://yhello.co
+ * Text Domain:       synchronizer
+ * Domain Path:       /languages
+ * Requires at least: 5.0
+ * Tested up to:      5.2.2
  *
- * @package         Wp_Sync
+ * @link              https://yhello.co
+ * @since             1.0
+ * @package           synchronizer
  */
 
-// Your code starts here.
+
+define( 'WP_SYNC_VERSION',               '0.0.1' );
+define( 'WP_SYNC_PRIVATE_KEY',           false );
+define( 'WP_SYNC_FILE',                  __FILE__ );
+define( 'WP_SYNC_PATH',                  realpath( plugin_dir_path( WP_SYNC_FILE ) ) . '/' );
+define( 'WP_SYNC_INC_PATH',              realpath( WP_SYNC_PATH . 'inc/' ) . '/' );
+
+require WP_ROCKET_INC_PATH . 'functions.php';
